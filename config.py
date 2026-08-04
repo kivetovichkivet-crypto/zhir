ef72e7ef24596b97c24f7b0bfdd98984a2c094c2
kivetovichkivet-crypto <kivetovichkivet@gmail.com>
2026-08-04 20:21:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+# Основные настройки проекта
 
+APP_NAME = "MyImageAI"
+VERSION = "0.1.0"
+
+# Модель (изменим позже)
+MODEL_ID = ""
+
+# Папки
+MODELS_DIR = "models"
+OUTPUTS_DIR = "outputs"
+
+# Настройки генерации
+DEFAULT_WIDTH = 1024
+DEFAULT_HEIGHT = 1024
+DEFAULT_STEPS = 25
+DEFAULT_GUIDANCE = 7.5
