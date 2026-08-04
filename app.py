43a03c8ac1123404517f60d446b9306193badd65
kivetovichkivet-crypto <kivetovichkivet@gmail.com>
2026-08-04 20:27:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from config import APP_NAME, VERSION
 
+from engine.logger import setup_logger
+from engine.utils import create_folders
+
 
 def main():
+    create_folders()
+
+    logger = setup_logger()
+
+    logger.info("Программа запущена")
+
     print(f"{APP_NAME} v{VERSION}")
-    print("Запуск...")
+    print("Все системы готовы")
 
 
 if __name__ == "__main__":
